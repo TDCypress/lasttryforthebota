f132299d25d3eb6d116c0962c077f637ad008e42
TDCypress <53574389+TDCypress@users.noreply.github.com>
2019-12-10 01:14:11 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,30 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "#";
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return message.channel.send("u dont have premission");
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : RafeekEhab`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : RafeekEhab ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setActivity(`made by Cypress`,"")
+});
+
+
+client.login("NjUzNzAxMjgwMzYyOTIyMDQ1.Xe7Ulw.y_uo_uMFw6BMU5mEWjjTc1db5Jg");
